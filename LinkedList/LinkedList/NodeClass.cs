8d82ad335a55447bfac000e30efd6074dcceaaca
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-12-28 19:03:33 -0800

    complete, testing all methods

---
+++ LinkedList/LinkedList/NodeClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class NodeClass
+    {
+        public int Value { get; set; }
+        internal NodeClass Next { get; set; }
+
+        public NodeClass(int value)
+        {
+            Value = value;
+        }
+    }
+}
