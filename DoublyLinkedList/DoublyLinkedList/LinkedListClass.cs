292eabd065efe38a7bcb1124100d1228af78b9d1
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-04 18:13:37 -0800

    created doubly linked list off singly linked list implementation

---
+++ DoublyLinkedList/DoublyLinkedList/LinkedListClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    public class LinkedListClass
+    {
+        public NodeClass Head { get; private set; }
+        private int ListSize { get; set; }
+
+        public LinkedListClass(NodeClass node)
+        {
+            Head = node;
+            ListSize = 1;
+        }
+
+        public void AddFirst(NodeClass newNode)
+        {
+            newNode.Next = Head;
+            Head = newNode;
+            Head.Next.Prev = Head;
+            ListSize++;
+        }
+
+        public void AddLast(NodeClass newNode)
+        {
+            NodeClass Current = Head;
+            while(Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+            Current.Next = newNode;
+            Current.Next.Prev = Current;
+            ListSize++;
+        }
+
+        public void AddBefore(NodeClass targetNode, NodeClass newNode)
+        {
+            NodeClass Current = Head;
+            while(Current.Next != targetNode)
+            {
+                Current = Current.Next;
+            }
+            newNode.Next = Current.Next;
+            Current.Next.Prev = newNode;
+            Current.Next = newNode;
+            newNode.Prev = Current;
+            ListSize++;
+        }
+
+        public void AddAfter(NodeClass targetNode, NodeClass newNode)
+        {
+            NodeClass Current = Head;
+            while(Current != targetNode)
+            {
+                Current = Current.Next;
+            }
+            newNode.Next = Current.Next;
+            Current.Next.Prev = newNode;
+            Current.Next = newNode;
+            newNode.Prev = Current;
+            ListSize++;
+        }
+
+        public void Remove(NodeClass targetNode)
+        {
+            NodeClass Current = Head;
+            while(Current != targetNode)
+            {
+                Current = Current.Next;
+            }
+            Current.Prev.Next = Current.Next;
+            Current.Next.Prev = Current.Prev;
+            Current = null;
+            ListSize--;
+        }
+    }
+}
---
+++ DoublyLinkedList/DoublyLinkedList/NodeClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    public class NodeClass
+    {
+        public int Value { get; set; }
+        internal NodeClass Next { get; set; }
+        internal NodeClass Prev { get; set; }
+
+        public NodeClass(int value)
+        {
+            Value = value;
+        }
+    }
+}
