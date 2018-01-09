6519207dd8e7883f3dc81bbd79ea1d485370a8b3
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-08 17:50:31 -0800

    implemented stacks and queues using linked lists

---
+++ StackAndQueue/StackAndQueue/Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    public class Queue
+    {
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+
+        public Queue(int n)
+        {
+            Head = new Node(n);
+            Tail = Head;
+        }
+
+        public void Enqueue(int n)
+        {
+            Node node = new Node(n);
+            if(Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.Next = Tail;
+                Tail.Prev = node;
+                Tail = node;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if(Head == Tail)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else
+            {
+                Head = Head?.Prev;
+            }
+        }
+
+        public int Peek() => Head.Value;
+    }
+}
