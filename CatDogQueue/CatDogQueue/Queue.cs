ab5501aeeda1fcecbe211afe953743314c37fbe2
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-09 18:03:11 -0800

    queue order is not preserved when removing an animal

---
+++ CatDogQueue/CatDogQueue/Queue.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatDogQueue
+{
+    class Queue
+    {
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        internal int Length { get; set; }
+
+        public Queue(string n)
+        {
+            Head = new Node(n);
+            Tail = Head;
+            Length = 1;
+        }
+
+        public void Enqueue(string n)
+        {
+            Node node = new Node(n);
+            if (n != "cat" && n != "dog")
+            {
+                Console.WriteLine("Can not enqueue something other than cat or dog.");
+                return;
+            }
+            else if (Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.Next = Tail;
+                Tail.Prev = node;
+                Tail = node;
+            }
+            Length++;
+        }
+
+        public void DequeueAny()
+        {
+            if (Head.Value == "cat") DequeueCat();
+            else DequeueDog();
+        }
+
+        public void DequeueCat()
+        {
+            if (Head.Value == "cat")
+            {
+                if (Head == Tail)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                Head = Head.Prev;
+                Head.Next = null;
+                }
+            }
+            else
+            {
+                string n = Head.Value;
+                n = Head.Value;
+                Enqueue(n);
+                Head = Head.Prev;
+                Head.Next = null;
+                DequeueCat();
+            }
+            Length--;
+        }
+
+        public void DequeueDog()
+        {
+            if (Head.Value == "dog")
+            {
+                if (Head == Tail)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                    Head = Head.Prev;
+                    Head.Next = null;
+                }
+            }
+            else
+            {
+                string n = Head.Value;
+                n = Head.Value;
+                Enqueue(n);
+                Head = Head.Prev;
+                Head.Next = null;
+                DequeueDog();
+            }
+            Length--;
+        }
+
+        public string Peek() => Head.Value;
+    }
+}
