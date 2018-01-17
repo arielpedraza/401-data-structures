749ad33b051eddebcfa1dde2803ff8510ed08946
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-17 13:55:17 -0800

    challenge complete

---
+++ Tree2/Tree2/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree2
+{
+    class Node
+    {
+        public char Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(char value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Tree2/Tree2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tree2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node A = new Node('A');
+            Node B = new Node('B');
+            Node C = new Node('C');
+            Node D = new Node('D');
+            Node E = new Node('E');
+            Node F = new Node('F');
+            Node G = new Node('G');
+
+            A.Left = B;
+            A.Right = C;
+            B.Left = D;
+            B.Right = E;
+            C.Left = F;
+            C.Right = G;
+
+            Console.WriteLine("          A          ");
+            Console.WriteLine("         / \\        ");
+            Console.WriteLine("        B   C        ");
+            Console.WriteLine("       / \\ / \\     ");
+            Console.WriteLine("      D  E F  G      ");
+
+            Tree breadthFirstTree = new Tree();
+            breadthFirstTree.BreadthFirst(A);
+            Console.ReadLine();
+        }
+    }
+}
