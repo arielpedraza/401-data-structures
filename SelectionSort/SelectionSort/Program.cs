14e3ebfbd6969f8fd74e5237b298685e97dd749c
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-12 14:28:27 -0800

    challenge completed

--- SelectionSort/SelectionSort/Program.cs
+++ SelectionSort/SelectionSort/Program.cs
@@ -6,7 +6,34 @@ namespace SelectionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unsorted:");
+            int[] array = new int[] {5,2,7,8,4};
+            foreach (int i in array) Console.Write(i + " ");
+            int[] sorted = SelectionSort(array);
+            Console.WriteLine("\nSorted:");
+            foreach (int i in sorted) Console.Write(i + " ");
+            Console.ReadLine();
+        }
+
+        static int[] SelectionSort(int[] array)
+        {
+            int[] myArray = array;
+            int temp, bucket;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                temp = i;
+                for (int k = i+1; k < myArray.Length; k++)
+                {
+                    if (myArray[temp] > myArray[k]) temp = k;
+                }
+                if(myArray[temp] < myArray[i])
+                {
+                    bucket = myArray[i];
+                    myArray[i] = myArray[temp];
+                    myArray[temp] = bucket;
+                }
+            }
+            return myArray;
         }
     }
 }
