7877f95ddde0ce7865f8d92c3d48b981547e0a3f
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-19 23:29:09 -0800

    Multiple attempts to fix algorithm. Still not working.

--- HeapSort/HeapSort/Program.cs
+++ HeapSort/HeapSort/Program.cs
@@ -6,22 +6,68 @@ namespace HeapSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 5, 3, 8, 2, 1, 9, 4, 6, 7 };
+            Console.WriteLine("Before HeapSort:");
+            Print(array);
+            Console.WriteLine("\n\nDuring HeapSort:");
+            HeapSort(array);
+            Console.WriteLine("\nAfter HeapSort:");
+            Print(array);
+            Console.ReadLine();
         }
 
         static void HeapSort(int[] array)
         {
-
+            BuildMaxHeap(array);
+            for(int i = array.Length - 1; i >= 0; i--)
+            {
+                Print(array);
+                Swap(array, 0, i);
+                Heapify(array, 0);
+                Console.WriteLine("");
+            }
         }
 
         static void BuildMaxHeap(int[] array)
         {
-
+            for (int i = (array.Length-2)/2; i >= 0; i--)
+            {
+                Heapify(array, i);
+            }
         }
 
         static void Heapify(int[] array, int i)
         {
+            int left = i * 2 + 1;
+            int right = i * 2 + 2;
+            int max = i;
 
+            if (left < array.Length && array[left] > array[i])
+            {
+                max = left;
+            }
+            if (right < array.Length && array[right] > array[max])
+            {
+                max = right;
+            }
+
+            if (max != i)
+            {
+                Swap(array, i, max);
+                Heapify(array, max);
+            }
+        }
+
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        static void Print(int[] array)
+        {
+            foreach (int i in array) Console.Write(i);
         }
     }
 }
