6899b90a7e57fe8b115ef2b20c472490e3dea008
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-19 20:39:00 -0800

    scaffold project for heapsort challenge

---
+++ HeapSort/HeapSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void HeapSort(int[] array)
+        {
+
+        }
+
+        static void BuildMaxHeap(int[] array)
+        {
+
+        }
+
+        static void Heapify(int[] array, int i)
+        {
+
+        }
+    }
+}
