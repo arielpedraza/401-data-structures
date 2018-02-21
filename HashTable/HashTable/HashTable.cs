4c7078add8d2cb8f550657f79eafa17e4115f38c
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-20 17:42:40 -0800

    Renamed app, namespace. Created HashTable class.

---
+++ HashTable/HashTable/HashTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableApp
+{
+    public class HashTable
+    {
+        private readonly int _tableSize;
+        private List<int>[] _hashTable;
+        
+        public HashTable(int expectedTableSize)
+        {
+            _tableSize = expectedTableSize;
+            _hashTable = new List<int>[expectedTableSize];
+        }
+
+
+    }
+}
