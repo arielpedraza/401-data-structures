858ce7560fcd6b53cdd32f7b0c8d12d26d5ef5b6
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-21 10:28:00 -0800

    Challenge complete

--- HashTable/HashTable/HashTable.cs
+++ HashTable/HashTable/HashTable.cs
@@ -7,32 +7,47 @@ namespace HashTableApp
     public class HashTable
     {
         private readonly int _tableSize;
-        private List<int>[] _hashTable;
+        private List<HashSet>[] _hashTable;
         
         public HashTable(int expectedTableSize)
         {
             _tableSize = expectedTableSize;
-            _hashTable = new List<int>[expectedTableSize];
+            _hashTable = new List<HashSet>[expectedTableSize];
             for (int i = 0; i < _hashTable.Length; i++)
             {
-                _hashTable[i] = new List<int>();
+                _hashTable[i] = new List<HashSet>();
             }
         }
 
         public void Add(string key, int value)
         {
             int hash = key.GetHashCode();
-            int index = hash % _tableSize;
-            _hashTable[index].Add(value);
+            int index = Math.Abs(hash % _tableSize);
+            _hashTable[index].Add(new HashSet { Key=key, Value=value });
         }
 
         public int Get(string key)
         {
             int hash = key.GetHashCode();
-            int index = hash % _tableSize;
-            foreach(int i in _hashTable[index])
+            int index = Math.Abs(hash % _tableSize);
+            foreach(HashSet x in _hashTable[index])
             {
-                if ()
+                if (x.Key == key) return x.Value;
+            }
+            return int.MinValue;
+        }
+
+        public void Print()
+        {
+            foreach(List<HashSet> list in _hashTable)
+            {
+                Console.Write("-");
+                foreach(HashSet set in list)
+                {
+                    if(set != null)
+                        Console.Write(set.Key + set.Value);
+                }
+                Console.WriteLine("");
             }
         }
     }
--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -7,6 +7,22 @@ namespace HashTableApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            HashTable hashTable = new HashTable(10);
+            hashTable.Add("Ariel", 10);
+            hashTable.Add("Jeff", 20);
+            hashTable.Add("Andres", 30);
+            hashTable.Add("Dustin", 40);
+            hashTable.Add("Luay", 50);
+            hashTable.Add("Zach", 60);
+            hashTable.Add("Austin", 70);
+            hashTable.Add("Amanda", 80);
+            hashTable.Add("Joy", 90);
+            hashTable.Add("Nicholas", 100);
+
+            hashTable.Print();
+            Console.WriteLine(hashTable.Get("Joy"));
+
+            Console.ReadLine();
         }
     }
 }
