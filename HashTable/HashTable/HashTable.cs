1a5e387b444d56633d0d4e5420b0d6d21e353559
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-20 18:43:44 -0800

    realized i need to store key value pair in list to account for collisions.

--- HashTable/HashTable/HashTable.cs
+++ HashTable/HashTable/HashTable.cs
@@ -13,8 +13,27 @@ namespace HashTableApp
         {
             _tableSize = expectedTableSize;
             _hashTable = new List<int>[expectedTableSize];
+            for (int i = 0; i < _hashTable.Length; i++)
+            {
+                _hashTable[i] = new List<int>();
+            }
         }
 
+        public void Add(string key, int value)
+        {
+            int hash = key.GetHashCode();
+            int index = hash % _tableSize;
+            _hashTable[index].Add(value);
+        }
 
+        public int Get(string key)
+        {
+            int hash = key.GetHashCode();
+            int index = hash % _tableSize;
+            foreach(int i in _hashTable[index])
+            {
+                if ()
+            }
+        }
     }
 }
